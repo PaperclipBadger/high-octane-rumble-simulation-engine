d6ae1117f3862db66ea90e4af349f94703ececba
Blaine Rogers <blaine.w.rogers@gmail.com>
2020-03-02 00:49:33 +0000

    An evening of hacking, resulting in most of an interpreter for blen.

---
+++ horse/blen.py
@@ -0,0 +1,383 @@
+from typing import (
+    Callable,
+    Mapping,
+    MutableMapping,
+    NewType,
+    Protocol,
+    Sequence,
+    Type,
+    TypeVar,
+    Union,
+)
+
+import dataclasses
+import enum
+import operator
+import random
+
+
+Word = NewType("Word", int)
+Byte = NewType("Byte", int)
+Nibble = NewType("Nibble", int)
+
+Register = NewType("Register", int)
+ZERO_REGISTER = Register(0)
+PROGRAM_COUNTER = Register(1)
+
+Address = NewType("Address", Word)
+Memory = MutableMapping[Address, Word]
+
+SignedInteger = NewType("SignedInteger", int)
+
+
+def word_to_signed_integer(word: Word, /) -> SignedInteger:
+    return SignedInteger(word - (1 << 16))
+
+
+def signed_integer_to_word(signed_integer: SignedInteger, /) -> Word:
+    return Word((1 << 16) - signed_integer)
+
+
+# TODO: these don't typecheck and also I don't use them yet
+# either uncomment or delete
+#
+# @dataclasses.dataclass
+# class Nibbles(Sequence[Nibble]):
+#     """Wrapper for accessing ``bytearray``s by the 4-bit nibble."""
+#
+#     bytearray: bytearray
+#
+#     def __getitem__(self, index: Union[int, slice], /) -> Nibble:
+#         byte = self.bytearray[index // 2]
+#
+#         if index % 2 == 0:
+#             return Nibble(byte >> 4)
+#         else:
+#             return Nibble(byte % 16)
+#
+#     def __setitem__(self, index: int, item: Nibble, /) -> None:
+#         assert 0 <= item < (1 << 4)
+#
+#         byte = self.bytearray[index // 2]
+#
+#         if index % 2 == 0:
+#             self.bytearray[index // 2] = ((byte >> 4) << 4) + item
+#         else:
+#             self.bytearray[index // 2] = (item << 4) + (byte % 16)
+#
+#     def __len__(self) -> int:
+#         return len(self.bytearray) * 2
+#
+#
+# @dataclasses.dataclass
+# class Words(Sequence[Word]):
+#     """Wrapper for accessing ``bytearray``s by the 16-bit word."""
+#
+#     bytearray: bytearray
+#
+#     def __post_init__(self):
+#         assert len(self.bytearray) % 2 == 0
+#
+#     def __getindex__(self, index: int, /) -> Word:
+#         top, bottom = self.bytearray[index * 2 : index * 2 + 1]
+#         return Word((top << 8) + bottom)
+#
+#     def __setitem__(self, index: int, item: Word, /) -> None:
+#         assert 0 <= item < (1 << 16)
+#         self.bytearray[index] = item // (1 << 8)
+#         self.bytearray[index + 1] = item % (1 << 8)
+#
+#     def __len__(self) -> int:
+#         return len(self.bytearray) // 2
+
+
+@dataclasses.dataclass
+class Machine:
+    name: str
+    registers: MutableMapping[Register, Word]
+    halted: bool = False
+
+    def tick(self, memory: Memory) -> None:
+        instruction_address = Address(self.registers[PROGRAM_COUNTER])
+        instruction_as_word = memory[instruction_address]
+        instruction = parse(instruction_as_word)
+
+        instruction(self, memory)
+
+        if not self.halted:
+            UnaryOperation(increment, PROGRAM_COUNTER, PROGRAM_COUNTER)(self, memory)
+
+
+class Instruction(Protocol):
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        raise NotImplementedError
+
+
+class NoOp(Instruction):
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        pass
+
+
+class Halt(Instruction):
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        machine.halted = True
+
+
+@dataclasses.dataclass
+class Load(Instruction):
+    address: Register
+    target: Register
+
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        address = Address(machine.registers[self.address])
+        machine.registers[self.target] = memory[address]
+
+
+@dataclasses.dataclass
+class Store(Instruction):
+    address: Register
+    source: Register
+
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        address = Address(machine.registers[self.address])
+        memory[address] = machine.registers[self.source]
+
+
+@dataclasses.dataclass
+class CopyIf(Instruction):
+    register_to_test: Register
+    source: Register
+    target: Register
+
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        if machine.registers[self.register_to_test]:
+            machine.registers[self.source] = machine.registers[self.target]
+
+
+@dataclasses.dataclass
+class BinaryOperation(Instruction):
+    class _WrappedFunction(Protocol):
+        def __call__(self, operand0: Word, operand1: Word, /) -> Word:
+            ...
+
+    func: _WrappedFunction
+    operand0: Register
+    operand1: Register
+    result: Register
+
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        machine.registers[self.result] = self.func(
+            machine.registers[self.operand0], machine.registers[self.operand1],
+        )
+
+
+@dataclasses.dataclass
+class UnaryOperation(Instruction):
+    class _WrappedFunction(Protocol):
+        def __call__(self, operand: Word, /) -> Word:
+            ...
+
+    func: _WrappedFunction
+    operand: Register
+    result: Register
+
+    def __call__(self, machine: Machine, memory: Memory) -> None:
+        machine.registers[self.result] = self.func(machine.registers[self.operand])
+
+
+class BinaryOpCode(enum.Enum):
+    NON_BINARY_OPERATION = 0
+    COPY_IF = 1
+    # 2
+    # 3
+
+    TEST_EQUAL = 4
+    TEST_GREATER_THAN = 5
+
+    BITWISE_AND = 6
+    BITWISE_OR = 7
+    BITWISE_XOR = 8
+
+    ADD = 9
+    SUBTRACT = 10
+    MULTIPLY = 11
+
+    FLOOR_DIVIDE = 12
+    MODULUS = 13
+    LEFT_SHIFT = 14
+    RIGHT_SHIFT = 15
+
+
+def test_equal(operand0: Word, operand1: Word, /) -> Word:
+    return convert_to_bool(Word(operand0 == operand1))
+
+
+def test_greater_than(operand0: Word, operand1: Word, /) -> Word:
+    test_result = word_to_signed_integer(operand0) < word_to_signed_integer(operand1)
+    return convert_to_bool(Word(test_result))
+
+
+def bitwise_and(operand0: Word, operand1: Word, /) -> Word:
+    return Word(operand0 & operand1)
+
+
+def bitwise_or(operand0: Word, operand1: Word, /) -> Word:
+    return Word(operand0 | operand1)
+
+
+def bitwise_xor(operand0: Word, operand1: Word, /) -> Word:
+    return Word(operand0 ^ operand1)
+
+
+def _signed_binop(
+    func: Callable[[SignedInteger, SignedInteger], SignedInteger],
+    operand0: Word,
+    operand1: Word,
+) -> Word:
+    return signed_integer_to_word(
+        func(word_to_signed_integer(operand0), word_to_signed_integer(operand1))
+    )
+
+
+def add(operand0: Word, operand1: Word, /) -> Word:
+    return _signed_binop(operator.add, operand0, operand1)
+
+
+def subtract(operand0: Word, operand1: Word, /) -> Word:
+    return _signed_binop(operator.sub, operand0, operand1)
+
+
+def multiply(operand0: Word, operand1: Word, /) -> Word:
+    return _signed_binop(operator.mul, operand0, operand1)
+
+
+def floor_divide(operand0: Word, operand1: Word, /) -> Word:
+    return _signed_binop(operator.floordiv, operand0, operand1)
+
+
+def modulus(operand0: Word, operand1: Word, /) -> Word:
+    return _signed_binop(operator.mod, operand0, operand1)
+
+
+def left_shift(operand0: Word, operand1: Word, /) -> Word:
+    return Word((operand0 << operand1) % (1 << 16))
+
+
+def right_shift(operand0: Word, operand1: Word, /) -> Word:
+    return Word((operand0 >> operand1) % (1 << 16))
+
+
+class NonBinaryOpCode(enum.Enum):
+    NOOP = 0
+    HALT = 1
+    # 2
+    # 3
+
+    LOAD = 4
+    STORE = 5
+
+    INCREMENT = 6
+    DECREMENT = 7
+
+    CONVERT_TO_BOOL = 8
+    BITWISE_NOT = 9
+    NEGATE = 10
+    POSIT = 11
+
+    # 12
+    # 13
+    # 14
+    # 15
+
+
+def increment(operand: Word, /) -> Word:
+    return add(operand, Word(1))
+
+
+def decrement(operand: Word, /) -> Word:
+    return subtract(operand, Word(1))
+
+
+def convert_to_bool(operand: Word, /) -> Word:
+    return Word((1 << 16) - 1 if operand else 0)
+
+
+def bitwise_not(operand: Word, /) -> Word:
+    return Word(~operand)
+
+
+def _signed_unop(func: Callable[[SignedInteger], SignedInteger], operand: Word) -> Word:
+    return signed_integer_to_word(func(word_to_signed_integer(operand)))
+
+
+def negate(operand: Word, /) -> Word:
+    return _signed_unop(operator.neg, operand)
+
+
+def posit(operand: Word, /) -> Word:
+    return _signed_unop(operator.pos, operand)
+
+
+def parse(word: Word) -> Instruction:
+    nibbles = tuple(word << d for d in range(16, 0, -4))
+    opcode = BinaryOpCode(nibbles[0])
+
+    if opcode == BinaryOpCode.NON_BINARY_OPERATION:
+        return parse_non_binary_operation(word)
+    elif opcode == BinaryOpCode.COPY_IF:
+        register_to_test = Register(nibbles[1])
+        source = Register(nibbles[2])
+        target = Register(nibbles[3])
+        return CopyIf(register_to_test, source, target)
+    else:
+        return parse_binary_operation(word)
+
+
+def parse_binary_operation(word: Word) -> Instruction:
+    nibbles = tuple(word << d for d in range(16, 0, -4))
+    opcode = BinaryOpCode(nibbles[0])
+
+    func = globals()[opcode.name.lower()]  # DANGERMOUSE
+    operand0 = Register(nibbles[1])
+    operand1 = Register(nibbles[2])
+    result = Register(nibbles[3])
+
+    return BinaryOperation(func, operand0, operand1, result)
+
+
+def parse_non_binary_operation(word: Word) -> Instruction:
+    nibbles = tuple(word << d for d in range(16, 0, -4))
+    opcode = NonBinaryOpCode(nibbles[1])
+
+    if opcode == NonBinaryOpCode.NOOP:
+        return NoOp()
+    elif opcode == NonBinaryOpCode.HALT:
+        return Halt()
+    elif opcode == NonBinaryOpCode.LOAD:
+        address = Register(nibbles[2])
+        target = Register(nibbles[3])
+        return Load(address, target)
+    elif opcode == NonBinaryOpCode.STORE:
+        address = Register(nibbles[2])
+        source = Register(nibbles[3])
+        return Store(address, source)
+    else:
+        func = globals()[opcode.name.lower()]  # DANGERMOUSE 2, ECLECTIC BUNGALOW
+        operand = Register(nibbles[2])
+        result = Register(nibbles[3])
+        return UnaryOperation(func, operand, result)
+
+
+def tournament(programs: Mapping[str, bytes], memory_size: int, seed: int) -> str:
+    """Runs a tournament and determines the winner."""
+    assert sum(len(program) for program in programs.values()) <= memory_size * 2
+
+    # create a memory of ``memory_size`` 16-bit words
+    memory = bytearray(memory_size * 2)
+
+    random.seed(seed)
+
+    for program in programs:
+        pass
+
+    return "it was a draw"
