cafd4f287eb067bc9d52bd07730bc560630a9640
Marcin Konowalczyk <marcin.konowalczyk@chem.ox.ac.uk>
2020-06-22 23:39:03 +0100

    Made tests work in terminal

--- tests/test_blen.py
+++ tests/test_blen.py
@@ -1,7 +1,12 @@
-import collections
-
+import unittest
 import hypothesis
 import hypothesis.strategies as st
+
+from collections import defaultdict
+
+# Add '.' to path so running this file by itself also works
+import os, sys
+sys.path.append(os.path.realpath('.'))
 
 import horse.blen
 import horse.types
@@ -18,7 +23,7 @@ in_range_signed_integers = st.integers(MIN_SIGNED_INT, MAX_SIGNED_INT)
 binary_operations = st.sampled_from(tuple(horse.blen.BINARY_OPERATIONS.values()))
 unary_operations = st.sampled_from(tuple(horse.blen.UNARY_OPERATIONS.values()))
 
-memories = st.dictionaries(words, words).map(lambda d: collections.defaultdict(int, d))
+memories = st.dictionaries(words, words).map(lambda d: defaultdict(int, d))
 machines = st.builds(
     horse.blen.Machine,
     memory=memories,
@@ -27,62 +32,58 @@ machines = st.builds(
     ),
 )
 
+class Tests(unittest.TestCase):
 
-@hypothesis.given(words)
-def test_signed_integer_encode_decode(word):
-    there = horse.blen.word_to_signed_integer(word)
-    back_again = horse.blen.signed_integer_to_word(there)
-    assert word == back_again, there
+    @hypothesis.given(words)
+    def test_signed_integer_encode_decode(self, word):
+        there = horse.blen.word_to_signed_integer(word)
+        back_again = horse.blen.signed_integer_to_word(there)
+        assert word == back_again, there
 
+    @hypothesis.given(binary_operations, words, words)
+    def test_binary_operations_return_words(self, func, operand0, operand1):
+        assert 0 <= func(operand0, operand1) <= horse.types.MAX_WORD
 
-@hypothesis.given(binary_operations, words, words)
-def test_binary_operations_return_words(func, operand0, operand1):
-    assert 0 <= func(operand0, operand1) <= horse.types.MAX_WORD
+    @hypothesis.given(unary_operations, words)
+    def test_unary_operations_return_words(self, func, operand):
+        assert 0 <= func(operand) <= horse.types.MAX_WORD
 
+    @hypothesis.given(
+        st.sampled_from((horse.blen.test_equal, horse.blen.test_greater_than)),
+        words,
+        words,
+    )
+    def test_comparison_functions_returns_bool_values(self, func, operand0, operand1):
+        result = func(operand0, operand1)
+        assert result == horse.types.TRUE or result == horse.types.FALSE
 
-@hypothesis.given(unary_operations, words)
-def test_unary_operations_return_words(func, operand):
-    assert 0 <= func(operand) <= horse.types.MAX_WORD
+    @hypothesis.given(words)
+    def test_convert_to_bool_returns_bool_values(self, operand):
+        result = horse.blen.convert_to_bool(operand)
+        assert result == horse.types.TRUE or result == horse.types.FALSE
 
+    @hypothesis.given(in_range_signed_integers)
+    def test_increment_non_overflow(self, operand):
+        result = horse.blen.increment(horse.blen.signed_integer_to_word(operand))
+        true_result = operand + 1
+        if MIN_SIGNED_INT <= true_result <= MAX_SIGNED_INT:
+            assert result == horse.blen.signed_integer_to_word(true_result)
 
-@hypothesis.given(
-    st.sampled_from((horse.blen.test_equal, horse.blen.test_greater_than)),
-    words,
-    words,
-)
-def test_comparison_functions_returns_bool_values(func, operand0, operand1):
-    result = func(operand0, operand1)
-    assert result == horse.types.TRUE or result == horse.types.FALSE
+    @hypothesis.given(in_range_signed_integers)
+    def test_decrement_non_overflow(self, operand):
+        result = horse.blen.decrement(horse.blen.signed_integer_to_word(operand))
+        true_result = operand - 1
+        if MIN_SIGNED_INT <= true_result <= MAX_SIGNED_INT:
+            assert result == horse.blen.signed_integer_to_word(true_result)
 
+    @hypothesis.given(words)
+    def test_parse_defined_for_all_words(self, word):
+        assert isinstance(horse.blen.parse(word), horse.blen.Instruction)
 
-@hypothesis.given(words)
-def test_convert_to_bool_returns_bool_values(operand):
-    result = horse.blen.convert_to_bool(operand)
-    assert result == horse.types.TRUE or result == horse.types.FALSE
+    @hypothesis.given(machines)
+    def test_register_zero_always_zero(self, machine):
+        machine.tick()
+        assert machine.registers[horse.blen.Register.ZERO_REGISTER] == 0
 
-
-@hypothesis.given(in_range_signed_integers)
-def test_increment_non_overflow(operand):
-    result = horse.blen.increment(horse.blen.signed_integer_to_word(operand))
-    true_result = operand + 1
-    if MIN_SIGNED_INT <= true_result <= MAX_SIGNED_INT:
-        assert result == horse.blen.signed_integer_to_word(true_result)
-
-
-@hypothesis.given(in_range_signed_integers)
-def test_decrement_non_overflow(operand):
-    result = horse.blen.decrement(horse.blen.signed_integer_to_word(operand))
-    true_result = operand - 1
-    if MIN_SIGNED_INT <= true_result <= MAX_SIGNED_INT:
-        assert result == horse.blen.signed_integer_to_word(true_result)
-
-
-@hypothesis.given(words)
-def test_parse_defined_for_all_words(word):
-    assert isinstance(horse.blen.parse(word), horse.blen.Instruction)
-
-
-@hypothesis.given(machines)
-def test_register_zero_always_zero(machine):
-    machine.tick()
-    assert machine.registers[horse.blen.Register.ZERO_REGISTER] == 0
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,21 +1,29 @@
+import unittest
 import hypothesis
 import hypothesis.strategies as st
 
+# Add '.' to path so running this file by itself also works
+import os, sys
+sys.path.append(os.path.realpath('.'))
 import horse.types
-
 
 words = st.integers(0, horse.types.MAX_WORD)
 
+class WordsToNibbles(unittest.TestCase):
 
-@hypothesis.given(words)
-def test_nibbles_returns_nibbles(word):
-    nibbles = horse.types.word_to_nibbles(word)
-    for nibble in nibbles:
-        assert 0 <= nibble <= horse.types.MAX_NIBBLE, nibbles
+    @hypothesis.given(words)
+    def test_word_to_nibbles(self, word):
+        ''' Nibbles return nibbles '''
+        nibbles = horse.types.word_to_nibbles(word)
+        for nibble in nibbles:
+            assert 0 <= nibble <= horse.types.MAX_NIBBLE, nibbles
 
+    @hypothesis.given(words)
+    def test_there_and_back(self, word):
+        ''' Nibbles to words and back '''
+        there = horse.types.word_to_nibbles(word)
+        back_again = horse.types.nibbles_to_word(there)
+        assert word == back_again, there
 
-@hypothesis.given(words)
-def test_nibbles_there_and_back_again(word):
-    there = horse.types.word_to_nibbles(word)
-    back_again = horse.types.nibbles_to_word(there)
-    assert word == back_again, there
+if __name__ == '__main__':
+    unittest.main()
